b60b6ab98341ce4eb1604cd543cb3d95f1674dab
sadasdasasd <32637304+avijxxx@users.noreply.github.com>
2025-12-31 21:10:34 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -10,12 +10,15 @@ sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $(find ./feeds/luci/modules/luci-m
 sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ $WRT_MARK-$WRT_DATE')/g" $(find ./feeds/luci/modules/luci-mod-status/ -type f -name "10_system.js")
 
 WIFI_FILE="./package/mtk/applications/mtwifi-cfg/files/mtwifi.sh"
-#修改WIFI名称
-sed -i "s/ImmortalWrt/$WRT_SSID/g" $WIFI_FILE
+#修改2.4G WIFI名称
+sed -i "s/ImmortalWrt-2.4G/$WRT_SSID_2G/g" $WIFI_FILE
+#修改5G WIFI名称
+sed -i "s/ImmortalWrt-5G/$WRT_SSID_5G/g" $WIFI_FILE
 #修改WIFI加密
-sed -i "s/encryption=.*/encryption='psk2+ccmp'/g" $WIFI_FILE
-#修改WIFI密码
-sed -i "/set wireless.default_\${dev}.encryption='psk2+ccmp'/a \\\t\t\t\t\t\set wireless.default_\${dev}.key='$WRT_WORD'" $WIFI_FILE
+sed -i "s/encryption=none/encryption=psk2+ccmp/g" $WIFI_FILE
+#修改2.4G WIFI密码
+sed -i "/ssid=\${ssid}$/a \\\t\t\t\t\tif [ \$idx -eq 1 ]; then key=\"$WRT_WORD_2G\"; else key=\"$WRT_WORD_5G\"; fi" $WIFI_FILE
+sed -i "/set wireless.default_\${dev}.encryption=psk2+ccmp/a \\\t\t\t\t\tset wireless.default_\${dev}.key=\${key}" $WIFI_FILE
 
 CFG_FILE="./package/base-files/files/bin/config_generate"
 #修改默认IP地址
